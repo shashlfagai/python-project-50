6cbd62cb6da2c1efcf1b251e69728db758e160d2
Loginov <itsmeegorloginov@gmail.com>
2023-08-15 21:07:40 +0300

    try and try and again and again

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -4,8 +4,4 @@ from gendiff.formaters.stylish import stylishing
 def serializing(diff):
     with open('diff.json', 'w') as output_file:
         output_file.write(stylishing(diff))
-    return (
-        'JSON file with diff is ready!\n'
-        'Path to file:\n'
-        '/python-project-50/diff.json'
-        )
+    return output_file
