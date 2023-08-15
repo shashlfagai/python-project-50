71c690375396e4aee12f2436ef8159cf721ae1ac
Loginov <itsmeegorloginov@gmail.com>
2023-08-15 21:11:44 +0300

    try and try and again and again

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -4,4 +4,4 @@ from gendiff.formaters.stylish import stylishing
 def serializing(diff):
     with open('diff.json', 'w') as output_file:
         output_file.write(stylishing(diff))
-    return output_file
+    return output_file.read()
