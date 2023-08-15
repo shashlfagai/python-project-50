bb721b59479dec4fe4e3af83ead739361d9f1e58
Loginov <itsmeegorloginov@gmail.com>
2023-08-15 22:18:08 +0300

    make again

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -1,7 +1,7 @@
-from gendiff.formaters.stylish import stylishing
+from gendiff.formaters.plain import plain
 
 
 def serializing(diff):
-    with open('diff.json', 'w') as output_file:
-        output_file.write(stylishing(diff))
-    return
+    with open('diff.json','w') as output_file:
+        output_file.write(plain(diff))
+    return output_file
