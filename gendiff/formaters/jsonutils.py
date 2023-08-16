66bab6264b6cb7b6be439fb644eb1601d8974eda
Loginov <itsmeegorloginov@gmail.com>
2023-08-16 23:24:29 +0300

    json should work

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -1,8 +1,9 @@
-from gendiff.formaters.plain import plain
+from gendiff.formaters.stylish import stylishing
 import json
 
 
 def serializing(diff):
+    formated_diff = stylishing(diff)
     with open('diff.json', 'w') as output_file:
-        json.dump(diff, output_file)
+        json.dump(formated_diff, output_file)
     return diff
