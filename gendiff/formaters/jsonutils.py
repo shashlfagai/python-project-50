b91fb236867803b6ed66391756d4badb01ea7283
Loginov <itsmeegorloginov@gmail.com>
2023-08-16 23:36:12 +0300

    ppc

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -3,7 +3,6 @@ import json
 
 
 def serializing(diff):
-    formated_diff = stylishing(diff)
     with open('diff.json', 'w') as output_file:
-        output_file.write(json.dumps(formated_diff))
-    return formated_diff
+        output_file.write(json.dumps(diff))
+    return diff
