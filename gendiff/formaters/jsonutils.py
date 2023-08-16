87242f0c4eb8bbecd0b9aa11e1c1c4549f14bff8
Loginov <itsmeegorloginov@gmail.com>
2023-08-16 20:04:41 +0300

    i dont know

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -3,7 +3,6 @@ import json
 
 
 def serializing(diff):
-    # formated_diff = plain(diff)
     with open('diff.json', 'w') as output_file:
         json.dump(diff, output_file, indent=2)
     return diff
