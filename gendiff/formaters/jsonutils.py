6ef90feb85b41ef8caf1403af95569a970059267
Loginov <itsmeegorloginov@gmail.com>
2023-08-17 00:01:26 +0300

    one more

--- gendiff/formaters/jsonutils.py
+++ gendiff/formaters/jsonutils.py
@@ -1,8 +1,9 @@
 from gendiff.formaters.stylish import stylishing
 import json
+import pickle
 
 
 def serializing(diff):
-    with open('diff.json', 'w') as output_file:
-        output_file.write(json.dumps(diff))
-    return diff
+    with open('diff.json', 'w') as data:
+        data = json.dumps(diff)
+    return data
